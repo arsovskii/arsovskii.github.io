9fd646a1396159d2a7806c677e00dafcb05bb70f
David Arsovski <arsovskidavid4@gmail.com>
2026-05-30 01:55:50 +0200

    fix

--- svelte.config.js
+++ svelte.config.js
@@ -21,7 +21,7 @@ const config = {
       strict: true,
     }),
     paths: {
-      base: process.env.NODE_ENV === "production" ? "/sveltekit-gh-pages" : "",
+      base: process.env.BASE_PATH || "",
     },
   },
 };
